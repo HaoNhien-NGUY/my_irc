186851792173c691283fb47073862934611c5acf
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-07-02 15:59:28 +0200

    added react build

---
+++ build/precache-manifest.34efccd53a30f50fe68a497581217648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be050ac92e7259c5384a900d5f7bcce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5fee503ca5dc4267bde",
+    "url": "/static/css/main.8cde13f8.chunk.css"
+  },
+  {
+    "revision": "75291293acdec5787698",
+    "url": "/static/js/2.9a7cac62.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.9a7cac62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fee503ca5dc4267bde",
+    "url": "/static/js/main.4b895add.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
